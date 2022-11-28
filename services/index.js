8e494b2c2fdb764fa34bf1016e25cd31e83090b3
TatsuyaHisama <kamiyanakamura@gmail.com>
2022-11-28 16:58:37 +0900

    create post ui

---
+++ services/index.js
@@ -0,0 +1,38 @@
+import { request , gql } from "graphql-request"
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async()=> {
+    const query = gql`
+        query MayQuery{
+            postsConnection {
+                    edges {
+                        node {
+                            author {
+                                id
+                                name
+                                biography
+                                picture {
+                                    url
+                                }
+                            }
+                            createdAt
+                            slug
+                            title
+                            excerpt
+                            featuredPost {
+                                url
+                            }
+                            category {
+                                name
+                                slug
+                            }
+                        }
+                    }
+                }
+            }
+        `;
+  const result = await request(graphqlAPI, query);
+
+  return result.postsConnection.edges;
+};
